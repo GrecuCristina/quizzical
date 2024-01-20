1a1112d5d6a1c679994f6ec806d310a4c8bb4020
GrecuCristina <grecu_cristina98@yahoo.com>
2024-01-20 18:54:34 +0200

    Update Intro.jsx

--- vite-project/src/components/Intro.jsx
+++ vite-project/src/components/Intro.jsx
@@ -2,7 +2,7 @@ export default function Intro(props) {
   return (
     <div className="intro">
       <h1 className="intro-description">Quizzical</h1>
-      <p className="intro-text">Some description if needed</p>
+      <p className="intro-text">Trivia game</p>
       <button className="btn intro-btn" onClick={props.onClick}>
         Start quiz
       </button>
